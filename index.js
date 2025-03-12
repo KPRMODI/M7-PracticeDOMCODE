a0810fa9c77a660921005f71f0c6bdeb37e000a0
KPRMODI <kpmodi@my.waketech.edu>
2025-03-12 13:06:41 -0400

    fixed stuff

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ let table = document.createElement("table");
 let tr1 = document.createElement("tr");
 let tr2 = document.createElement("tr");
 let tr3 = document.createElement("tr");
-
-
 let test = document.getElementById("test")
-let text1 = document.getElementById("td")
-console.log(text1)
+// creates elements and links div to javacript
+
 test.appendChild(table)
 table.appendChild(tr1)
 table.appendChild(tr2)
 table.appendChild(tr3)
+// appends table and tr's to the div, creating the table
 
 const useText = ["Car", "Chevrolet", "Pontiac", "Top", "120mph", "140mph", "Price", "$10,000","$20,000"];
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 9; i++) { // this for loop simplifies the process appending text to the tr's 
     let td = document.createElement("td");
     td.textContent = useText[i];
     table.children[i%3].appendChild(td);
 }
 
-test.style.backgroundColor = "rgb(125, 125, 125)" // style by .style
-test.style.border = "5px dotted rgb(0, 0, 0)" // style by .style
+test.style.backgroundColor = "rgb(125, 125, 125)" // styles the table making the background grey
+test.style.border = "2px solid rgb(0,0,255)" // style the table by making a border around it with a blue color
 
-test.setAttribute("style", (test.getAttribute("style") || "") + "width: fit-content; padding: 15px;")  // style by setAttribute
+test.setAttribute("style", (test.getAttribute("style") || "") + "border-radius: 15px;")  // style by setAttribute, make the border look circular
+test.setAttribute("style", (test.getAttribute("style") || "") + "width: 200px;")  // style by setAttribute, makes the width of the table smaller
